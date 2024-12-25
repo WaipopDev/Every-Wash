35cade2a38902b9c2f93ec6802e22899715e4f41
Waipop B <>
2024-12-25 15:37:53 +0700

    no message

--- backend/pages/_document.js
+++ backend/pages/_document.js
@@ -6,10 +6,10 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <title>Ever Wash System</title>
+          <title>Every Wash System</title>
           <meta charSet="utf-8" />
-          <meta name="keywords" content="Ever Wash System"></meta>
-          <meta name="description" content="Ever Wash System"></meta>
+          <meta name="keywords" content="Every Wash System"></meta>
+          <meta name="description" content="Every Wash System"></meta>
           <meta name="theme-color" content="#000000" />
           {/* <link rel="icon" href="/favicon.ico" /> */}
 
